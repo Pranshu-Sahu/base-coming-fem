f10754efea7a5a32340feea5ac5b6dd4a4d5bae4
Pranshu Sahu <sahupranshu637@gmail.com>
2022-10-30 20:34:10 +0530

    solution 1

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./*.{html,js}'],
+  theme: {
+    extend: {
+      colors:{
+        desaturatedRed:'hsl(0, 36%, 70%)',
+        softRed: 'hsl(0, 93%, 68%)',
+        DarkGrayishRed: 'hsl(0, 6%, 24%)'
+      }
+    },
+  },
+  plugins: [],
+}
